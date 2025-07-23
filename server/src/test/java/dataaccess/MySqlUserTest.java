5ae128a0a0c319a9351ae97d32deb2f4f34d9dd1
Amanda Warren <aw998@byu.edu>
2025-07-22 20:29:53 -0500

    SQL UserTests working

--- server/src/test/java/dataaccess/MySqlUserTest.java
+++ server/src/test/java/dataaccess/MySqlUserTest.java
@@ -29,7 +29,7 @@ class MySqlUserTest {
     }
 
     @Test
-    @DisplayName("Positive CreateUser")
+    @DisplayName("Positive createUser")
     void createUser() throws Exception {
         String username = "firstUser";
         String password = "MyPassword123!!!";
@@ -41,6 +41,7 @@ class MySqlUserTest {
         // Retrieve user
         UserData userData = userDao.getUser(username);
 
+        // Assertions
         assertNotNull(userData);
         assertEquals(username, userData.username());
         assertEquals(email, userData.email());
@@ -50,10 +51,76 @@ class MySqlUserTest {
     }
 
     @Test
-    void getUser() {
+    @DisplayName ("Username already taken")
+    void createInvalidUser() throws Exception {
+        String username = "firstUser";
+        String password = "MyPassword123!!!";
+        String email = "firstUser@gmail.com";
+
+        String otherUsername = "firstUser";
+        String otherPassword = "newpassword";
+        String otherEmail = "newemail@gmail.com";
+
+        // Create first user
+        userDao.createUser(username, password, email);
+
+        // Attempt to create second user with same username
+        AlreadyTakenException ex = assertThrows(AlreadyTakenException.class, () -> {
+            userDao.createUser(otherUsername, otherPassword, otherEmail);
+        });
+        System.out.print("Exception message: " + ex.getMessage());
+
+        // Assertions
+        assertTrue(ex.getCause().getMessage().contains("already taken"));
     }
 
     @Test
-    void clearAllUsers() {
+    @DisplayName("Positive getUser")
+    void getUser() throws Exception {
+        String username = "myUsername";
+        String password = "password123";
+        String email = "email@gmail.com";
+
+        // Create a user
+        userDao.createUser(username, password, email);
+
+        // Get the user
+        UserData userData = userDao.getUser("myUsername");
+
+        // Assertions
+        assertNotNull(userData);
+        assertEquals(username, userData.username());
+        assertEquals(email, userData.email());
+        assertNotEquals(password, userData.password());
+
+        assertTrue(checkpw(password, userData.password()));
+    }
+
+    @Test
+    @DisplayName("Get Username not in Database")
+    void getInvalidUserReturnsNull() throws Exception {
+        // Try to get user from empty database
+        UserData userData = userDao.getUser("myUsername");
+
+        // Assertions
+        assertNull(userData);
+    }
+
+    @Test
+    void clearAllUsers() throws Exception {
+        // Put users in the database
+        userDao.createUser("Bella", "forks", "bellaswan@gmail.com");
+        userDao.createUser("Edward", "vampire", "edwardcullen@gmail.com");
+
+        // Make sure they exist
+        assertNotNull(userDao.getUser("Bella"));
+        assertNotNull(userDao.getUser("Edward"));
+
+        // Clear users
+        userDao.clearAllUsers();
+
+        // Check database
+        assertNull(userDao.getUser("Bella"));
+        assertNull(userDao.getUser("Edward"));
     }
 }
